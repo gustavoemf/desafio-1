cd7689812797fd1de5e8b6f12ee9b8ad06bef212
gustavoemf <gustavoeduardomf@gmail.com>
2023-07-31 14:59:55 -0400

    Cadastro ok

--- src/main/java/org/example/Functions.java
+++ src/main/java/org/example/Functions.java
@@ -12,6 +12,36 @@ public class Functions {
         System.out.println("1 - Cadastrar nova pessoa");
         System.out.println("2 - Procurar pessoa");
         System.out.println("3 - Sair");
+        System.out.print("Selecione a opção desejada: ");
         return scanner.nextInt();
+    }
+
+    public List<Pessoa> cadastrar(){
+        List<Pessoa> pessoas = new ArrayList<>();
+
+        System.out.println("Insira quantas pessoas gostaria de cadastrar no sistema: ");
+
+        int quantPessoas = scanner.nextInt();
+
+        for(int i = 0; i < quantPessoas; i++){
+            List<Endereco> enderecos = new ArrayList<>();
+            System.out.print("Insira o nome da pessoa " + (i+1) + ": ");
+            String nome = scanner.next();
+            System.out.print("Insira a idade da pessoa " + (i+1) + ": ");
+            int idade = scanner.nextInt();
+            System.out.print("Insira quantos endereços a pessoa " + (i+1) + " terá: ");
+            int qntdEnd = scanner.nextInt();
+            for (int j = 0; j < qntdEnd; j++) {
+                System.out.println("--- Endereço número" + (j + 1));
+                System.out.print("Insira o nome da rua: ");
+                String nomeRua = scanner.next();
+                System.out.print("Insira o número da residência: ");
+                int numero = scanner.nextInt();
+                enderecos.add(new Endereco(nomeRua, numero));
+            }
+            pessoas.add(new Pessoa(nome, idade, enderecos));
+        }
+
+        return pessoas;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Functions functions = new Functions();
+        List<Pessoa> pessoas = new ArrayList<>();
+        int option;
+
+        do{
+            option = functions.menu();
+
+            switch (option){
+                case 1 -> pessoas = functions.cadastrar();
+                case 3 -> {
+                    System.out.print("\nSaindo...\n");
+                    System.exit(0);
+                }
+                default -> System.out.println("\nOpção inválida, tente novamente\n");
+            }
+        }while(option != 3);
+
     }
 }
